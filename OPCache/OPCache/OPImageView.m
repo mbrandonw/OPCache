344d7729e9a41849bd0b4a2fcf6398ef6edaf8bc
Brandon Williams <brandon@kickstarter.com>
2013-10-19 19:13:03 -0400

    Converted to 2 space tabs.

--- OPCache/OPCache/OPImageView.m
+++ OPCache/OPCache/OPImageView.m
@@ -30,108 +30,108 @@
 }
 
 -(void) cancel {
-    [[OPCache sharedCache] cancelFetchForHandle:self.cancelHandle];
-    self.cancelHandle = nil;
+  [[OPCache sharedCache] cancelFetchForHandle:self.cancelHandle];
+  self.cancelHandle = nil;
 }
 
 -(void) loadImageURL:(NSString*)url {
-    [self loadImageURL:url placeholder:nil];
+  [self loadImageURL:url placeholder:nil];
 }
 
 -(void) loadImageURL:(NSString*)url placeholder:(UIImage*)placeholder {
-    [self loadImageURL:url placeholder:placeholder cacheName:nil processing:nil];
+  [self loadImageURL:url placeholder:placeholder cacheName:nil processing:nil];
 }
 
 -(void) loadImageURL:(NSString*)url placeholder:(UIImage*)placeholder cacheName:(NSString*)cacheName processing:(UIImage*(^)(UIImage *image))processing {
-    [self loadImageURL:url placeholder:placeholder cacheName:cacheName processing:processing completion:nil];
+  [self loadImageURL:url placeholder:placeholder cacheName:cacheName processing:processing completion:nil];
 }
 
--(void) loadImageURL:(NSString*)url 
-         placeholder:(UIImage*)placeholder 
-           cacheName:(NSString*)cacheName 
+-(void) loadImageURL:(NSString*)url
+         placeholder:(UIImage*)placeholder
+           cacheName:(NSString*)cacheName
           processing:(UIImage*(^)(UIImage *image))processing
           completion:(OPCacheImageCompletionBlock)completion {
-    
-    self.image = nil;
-    self.imageURL = url;
-    [self cancel];
-    if (placeholder) {
-        self.placeholderImageView.image = placeholder;
-        self.placeholderImageView.alpha = 1.0f;
-    } else {
-        _placeholderImageView.image = nil;
+
+  self.image = nil;
+  self.imageURL = url;
+  [self cancel];
+  if (placeholder) {
+    self.placeholderImageView.image = placeholder;
+    self.placeholderImageView.alpha = 1.0f;
+  } else {
+    _placeholderImageView.image = nil;
+  }
+
+  self.cancelHandle = [[OPCache sharedCache] fetchImageForURL:url cacheName:cacheName processing:processing completion:^(UIImage *image, BOOL fromCache) {
+
+    self.image = image;
+    [self setNeedsLayout];
+    [self.superview setNeedsLayout];
+    self.cancelHandle = nil;
+
+    if (completion) {
+      completion(image, fromCache);
     }
-    
-    self.cancelHandle = [[OPCache sharedCache] fetchImageForURL:url cacheName:cacheName processing:processing completion:^(UIImage *image, BOOL fromCache) {
-        
-        self.image = image;
-        [self setNeedsLayout];
-        [self.superview setNeedsLayout];
-        self.cancelHandle = nil;
-        
-        if (completion) {
-            completion(image, fromCache);
-        }
-        
-        if (placeholder) {
-            _placeholderImageView.alpha = 1.0f;
-        } else {
-            self.alpha = 0.0f;
-        }
-        BOOL animate = self.animation == OPImageViewAnimationFade || (self.animation == OPImageViewAnimationAuto && [self deviceIsFast]);
-        [UIView animateWithDuration:0.3f*(!fromCache)*animate animations:^{
-            if (placeholder) {
-                _placeholderImageView.alpha = 0.0f;
-            } else {
-                self.alpha = 1.0f;
-            }
-        }];
+
+    if (placeholder) {
+      _placeholderImageView.alpha = 1.0f;
+    } else {
+      self.alpha = 0.0f;
+    }
+    BOOL animate = self.animation == OPImageViewAnimationFade || (self.animation == OPImageViewAnimationAuto && [self deviceIsFast]);
+    [UIView animateWithDuration:0.3f*(!fromCache)*animate animations:^{
+      if (placeholder) {
+        _placeholderImageView.alpha = 0.0f;
+      } else {
+        self.alpha = 1.0f;
+      }
     }];
+  }];
 }
 
 -(void) didMoveToWindow {
-    if (! self.window) {
-        [self cancel];
-    }
+  if (! self.window) {
+    [self cancel];
+  }
 }
 
 -(UIImageView*) placeholderImageView {
-    if (! _placeholderImageView)
-    {
-        self.placeholderImageView = [UIImageView new];
-        self.placeholderImageView.frame = self.bounds;
-        self.placeholderImageView.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
-        [self addSubview:self.placeholderImageView];
-    }
-    return _placeholderImageView;
+  if (! _placeholderImageView)
+  {
+    self.placeholderImageView = [UIImageView new];
+    self.placeholderImageView.frame = self.bounds;
+    self.placeholderImageView.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+    [self addSubview:self.placeholderImageView];
+  }
+  return _placeholderImageView;
 }
 
 -(BOOL) deviceIsFast {
-    static NSInteger fastFlag = -1;
-    if (fastFlag >= 0) {
-        return fastFlag == 1;
-    }
-    
-#if TARGET_IPHONE_SIMULATOR
-    fastFlag = 1;
-    return YES;
-#endif
-    
-    size_t size;
-    sysctlbyname("hw.machine", NULL, &size, NULL, 0);
-    char *answer = malloc(size);
-    sysctlbyname("hw.machine", answer, &size, NULL, 0);
-    NSString *platform = [NSString stringWithCString:answer encoding: NSUTF8StringEncoding];
-    free(answer);
-    
-    if ([platform hasPrefix:@"iPhone"]) {
-        fastFlag = [platform compare:@"iPhone4"] == NSOrderedDescending ? 1 : 0;
-    } else if ([platform hasPrefix:@"iPod"]) {
-        fastFlag = [platform compare:@"iPod5"] == NSOrderedDescending ? 1 : 0;
-    } else if ([platform hasPrefix:@"iPad"]) {
-        fastFlag = [platform compare:@"iPad3"] == NSOrderedDescending ? 1 : 0;
-    }
+  static NSInteger fastFlag = -1;
+  if (fastFlag >= 0) {
     return fastFlag == 1;
+  }
+
+#if TARGET_IPHONE_SIMULATOR
+  fastFlag = 1;
+  return YES;
+#endif
+
+  size_t size;
+  sysctlbyname("hw.machine", NULL, &size, NULL, 0);
+  char *answer = malloc(size);
+  sysctlbyname("hw.machine", answer, &size, NULL, 0);
+  NSString *platform = [NSString stringWithCString:answer encoding: NSUTF8StringEncoding];
+  free(answer);
+
+  if ([platform hasPrefix:@"iPhone"]) {
+    fastFlag = [platform compare:@"iPhone4"] == NSOrderedDescending ? 1 : 0;
+  } else if ([platform hasPrefix:@"iPod"]) {
+    fastFlag = [platform compare:@"iPod5"] == NSOrderedDescending ? 1 : 0;
+  } else if ([platform hasPrefix:@"iPad"]) {
+    fastFlag = [platform compare:@"iPad3"] == NSOrderedDescending ? 1 : 0;
+  }
+  return fastFlag == 1;
 }
 
 @end
